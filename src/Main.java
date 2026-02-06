0b312bc5522894e719525daa4fb738e578fdb5e6
juandlondono <juandiegolondono2004@gmail.com>
2026-02-06 09:39:59 -0500

    archivos pendientes

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        CuentaBancaria cuenta1 = new CuentaBancaria("1", 100);
+        cuenta1.retirar(100);
+        cuenta1.consignar(150);
+        System.out.println("El saldo de la cuenta es: " + cuenta1.getSaldo());
+
+        Notificacion[] notificaciones = new Notificacion[2];
+        notificaciones[0] = new NotificacionCorreo("Pepito");
+        notificaciones[1] = new NotificacionSMS("Juanito");
+
+        notificaciones[0].enviarMensaje("Hola mi amigo");
+        notificaciones[1].enviarMensaje("Hola mi broco");
+    }
+}
